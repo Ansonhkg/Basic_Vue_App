468d5f50fd0c0078258fab07fd5f318270b4f16c
Anson <ansonox@gmail.com>
2018-02-18 22:50:06 +0000

    Add Header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,24 @@
 <template>
     <div>
-        Test
+        <h1>src/components/Header.vue</h1>
+        <hr>
+        <router-link to="/login" v-if="!isAuth">
+            <a>Login</a>
+        </router-link>
     </div>
 </template>
+
+<script>
+export default {
+    data(){
+        return{
+            isActive: true
+        }
+    },
+    computed:{
+        isAuth(){
+            return false
+        }
+    }
+}
+</script>
