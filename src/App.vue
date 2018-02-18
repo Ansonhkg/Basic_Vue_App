479aec5ea95f104600474afad25a541c3780b206
Anson <ansonox@gmail.com>
2018-02-18 23:13:40 +0000

    Add Footer

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <template>
-  <div id="app">
-    <vue-header></vue-header>
-    <router-view/>
-  </div>
+	<div id="app">
+		<vue-header></vue-header>
+		<router-view/>
+		<div class="footer">
+			<p>&copy; Copyright {{software_name}} {{length}}</p>
+		</div>
+	</div>
 </template>
 
 <script>
@@ -10,10 +13,28 @@
 import Header from '@/components/layouts/Header'
 
 export default {
-  name: 'App',
-  components:{
-    'vue-header' : Header,
-  }
+	data(){
+		return{
+			software_name: 'Gym Connect',
+			startYear: 2016,
+			currentYear: null
+		}
+	},
+	created(){
+		this.currentYear = new Date().getFullYear()
+	},
+	computed:{
+		length(){
+			if(this.startYear === this.currentYear){
+				return this.currentYear
+			}
+			return this.startYear + ' - ' + this.currentYear
+		}
+	},
+	name: 'App',
+	components:{
+		'vue-header' : Header,
+	}
 }
 </script>
 
